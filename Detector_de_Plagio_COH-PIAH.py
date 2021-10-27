8aecd1c0d049f1521711d2a647416510cc777a8c
Raphael-Aguiar <raphael.aguiar@gmail.com>
2021-10-26 21:51:36 -0300

    Versão aprovada com nota 8/10 (a comparação de duas assinaturas deu erro)

--- Detector_de_Plagio_COH-PIAH.py
+++ Detector_de_Plagio_COH-PIAH.py
@@ -37,11 +37,12 @@ def separa_sentencas(texto):
 
 def separa_frases(sentenca):
     '''A funcao recebe uma sentenca e devolve uma lista das frases dentro da sentenca'''
+    #sentenca = str(sentenca)
     return re.split(r'[,:;]+', sentenca)
 
 def separa_palavras(frase):
     '''A funcao recebe uma frase e devolve uma lista das palavras dentro da frase'''
-    frase = str(frase)
+    #frase = str(frase)
     return frase.split()
 
 def n_palavras_unicas(lista_palavras):
@@ -74,84 +75,115 @@ def n_palavras_diferentes(lista_palavras):
 
     return len(freq)
 
-def tamanho_medio_da_sentença(sentenca):
-    media_caracteres_por_sentença = 0
-    palavra1 = []
-    frases_temp = separa_frases(sentenca)
-    for frase in frases_temp:
-            palavra1.extend(separa_palavras(frase))
-            for palavra in palavra1:
-                media_caracteres_por_sentença = media_caracteres_por_sentença + len(palavra)
-            palavra1 = []
-    return media_caracteres_por_sentença
-
-def tamanho_medio_da_frase(frase):
-    media_caracteres_por_frase = 0
-    palavras_temp = separa_palavras(frase)
-    for palavra in palavras_temp:
-        media_caracteres_por_frase = media_caracteres_por_frase + len(palavra)
-    return media_caracteres_por_frase
-
 def compara_assinatura(as_a, as_b):
     '''IMPLEMENTAR. Essa funcao recebe duas assinaturas de texto e deve devolver o grau de similaridade nas assinaturas.'''
-    pass
-
-
+    
+    diferenca = [x - y for x, y in zip(as_a, as_b)]
+    diferenca = (sum(diferenca)) / 6
+    return abs(diferenca)
+    
 def avalia_textos(textos, ass_cp):
     '''IMPLEMENTAR. Essa funcao recebe uma lista de textos e uma assinatura ass_cp e deve devolver o numero (1 a n) do texto com maior probabilidade de ter sido infectado por COH-PIAH.'''
-    pass
+    assinaturas_extraidas = []
+    comparacoes = []
+    for i in textos:
+        assinaturas_extraidas.append(calcula_assinatura(i))
+    for n in assinaturas_extraidas:
+        comparacoes.append(compara_assinatura(n, ass_cp))
+    minima = min(comparacoes)
+    para_retorno = comparacoes.index(minima)
+    return para_retorno + 1
 
 def calcula_assinatura(texto):
     '''IMPLEMENTAR. Essa funcao recebe um texto e deve devolver a assinatura do texto.'''
+
+    # função calcula-assinatura quase pronta. Falta consertar #4 - Tamanho Médio de Sentença e #6 - Tamanho Médio de Frase!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     
     #Declaração de variáveis:
     lista_de_frases = []
     lista_de_palavras = []
-    total_caracteres = 0
+    total_letras = 0
     media_caracteres_por_sentença = 0
     tamanho_medio_sentenca = 0
-    tms = 0
-    tmf = 0
+    tamanho_medio_frase = 0
+    
     # Separação das sentenças do texto:
     lista_de_sentencas = separa_sentencas(texto)
+    
     # Loopings para popular listas de frases e de palavras:
+    total_caracteres_sentenca = 0
+    total_caracteres_frase = 0
     for sentenca in lista_de_sentencas:
         lista_de_frases.extend(separa_frases(sentenca))
+    for i in lista_de_sentencas:
+        total_caracteres_sentenca = total_caracteres_sentenca + len(i)
     for frase in lista_de_frases:
         lista_de_palavras.extend(separa_palavras(frase))
+    for i in lista_de_frases:
+        total_caracteres_frase = total_caracteres_frase + len(i)
+    
     # Contagem dos caracteres totais sem espaço:
     for palavra in lista_de_palavras:
-        total_caracteres = total_caracteres + len(palavra)
+        total_letras = total_letras + len(palavra)
+    
     # Cálculos:
+    
     ## 0. Número total de palavras:
     total_palavras = len(lista_de_palavras)
+    
     ## 1. Tamanho médio de palavra: soma dos tamanhos das palavras dividida pelo número total de palavras (Média simples do número de caracteres por palavra).
-    tamanho_medio_palavras = total_caracteres / total_palavras
+    tamanho_medio_palavras = total_letras / total_palavras
+    
     ## 2. Relação Type-Token: Número de palavras diferentes utilizadas em um texto divididas pelo total de palavras.
     type_token = n_palavras_diferentes(lista_de_palavras) / total_palavras
+    
     ## 3. Razão Hapax Legomana: Número de palavras utilizadas uma única vez dividido pelo número total de palavras
     hapax_legomana = n_palavras_unicas(lista_de_palavras) / total_palavras
+    
     ## 4. Tamanho médio de sentença: Média simples do número de caracteres por sentença.
-    for sentenca in lista_de_sentencas:
-        tms = tms + tamanho_medio_da_sentença(sentenca)
-    tamanho_medio_sentenca = tms / len(lista_de_sentencas)
-        # tamanho_medio_sentenca = total_caracteres / len(lista_de_sentencas)
-                # Soma de todos os caracteres de todas as sentenças / No de sentenças
-                # (os caracteres que separam uma sentença da outra não devem ser contabilizados como parte da sentença)
+    tamanho_medio_sentenca = total_caracteres_sentenca / len(lista_de_sentencas)
+    
     ## 5. Complexidade de sentença: Média simples do número de frases por sentença (Número total de frases divido pelo número de sentenças)
     complexidade_de_sentença = len(lista_de_frases) / len(lista_de_sentencas)
+    
     ## 6. Tamanho médio de frase: Média simples do número de caracteres por frase (Soma do número de caracteres em cada frase dividida pelo número de frases no texto)
-    for frase in lista_de_frases:
-        tmf = tmf + tamanho_medio_da_frase(frase)
-    tamanho_medio_frase = tmf / len(lista_de_frases)
-            # (os caracteres que separam uma frase da outra não devem ser contabilizados como parte da frase).
-                # Soma do No total de caracteres / len(lista_de_frases)
+    tamanho_medio_frase = total_caracteres_frase / len(lista_de_frases)
+    
     # Retorno do resultado final
     assinatura = [tamanho_medio_palavras, type_token, hapax_legomana, tamanho_medio_sentenca, complexidade_de_sentença, tamanho_medio_frase]
     return assinatura
 
-texto = "Então resolveu ir brincar com a Máquina pra ser também imperador dos filhos da mandioca. Mas as três cunhas deram muitas risadas e falaram que isso de deuses era gorda mentira antiga, que não tinha deus não e que com a máquina ninguém não brinca porque ela mata. A máquina não era deus não, nem possuía os distintivos femininos de que o herói gostava tanto. Era feita pelos homens. Se mexia com eletricidade com fogo com água com vento com fumo, os homens aproveitando as forças da natureza. Porém jacaré acreditou? nem o herói! Se levantou na cama e com um gesto, esse sim! bem guaçu de desdém, tó! batendo o antebraço esquerdo dentro do outro dobrado, mexeu com energia a munheca direita pras três cunhas e partiu. Nesse instante, falam, ele inventou o gesto famanado de ofensa: a pacova."
-print(calcula_assinatura(texto))
+
+ass_cp = le_assinatura()
+
+textos = le_textos()
+
+resultado = avalia_textos(textos, ass_cp)
+
+print("o autor do texto", resultado, "está infectado com COH-PIAH")
 
 
+# Área de Testes ***************************************************************
+
+#teste = avalia_textos(['Então resolveu ir brincar com a Máquina pra ser também imperador dos filhos da mandioca. Mas as três cunhas deram muitas risadas e falaram que isso de deuses era gorda mentira antiga, que não tinha deus não e que com a máquina ninguém não brinca porque ela mata. A máquina não era deus não, nem possuía os distintivos femininos de que o herói gostava tanto. Era feita pelos homens. Se mexia com eletricidade com fogo com água com vento com fumo, os homens aproveitando as forças da natureza. Porém jacaré acreditou? nem o herói! Se levantou na cama e com um gesto, esse sim! bem guaçu de desdém, tó! batendo o antebraço esquerdo dentro do outro dobrado, mexeu com energia a munheca direita pras três cunhas e partiu. Nesse instante, falam, ele inventou o gesto famanado de ofensa: a pacova.', 'Voltei-me para ela; Capitu tinha os olhos no chão. Ergueu-os logo, devagar, e ficamos a olhar um para o outro... Confissão de crianças, tu valias bem duas ou três páginas, mas quero ser poupado. Em verdade, não falamos nada; o muro falou por nós. Não nos movemos, as mãos é que se estenderam pouco a pouco, todas quatro, pegando-se, apertando-se, fundindo-se. Não marquei a hora exata daquele gesto. Devia tê-la marcado; sinto a falta de uma nota escrita naquela mesma noite, e que eu poria aqui com os erros de ortografia que trouxesse, mas não traria nenhum, tal era a diferença entre o estudante e o adolescente. Conhecia as regras do escrever, sem suspeitar as do amar; tinha orgias de latim e era virgem de mulheres.', 'Num fabulário ainda por encontrar será um dia lida esta fábula: A uma bordadora dum país longínquo foi encomendado pela sua rainha que bordasse, sobre seda ou cetim, entre folhas, uma rosa branca. A bordadora, como era muito jovem, foi procurar por toda a parte aquela rosa branca perfeitíssima, em cuja semelhança bordasse a sua. Mas sucedia que umas rosas eram menos belas do que lhe convinha, e que outras não eram brancas como deviam ser. Gastou dias sobre dias, chorosas horas, buscando a rosa que imitasse com seda, e, como nos países longínquos nunca deixa de haver pena de morte, ela sabia bem que, pelas leis dos contos como este, não podiam deixar de a matar se ela não bordasse a rosa branca. Por fim, não tendo melhor remédio, bordou de memória a rosa que lhe haviam exigido. Depois de a bordar foi compará-la com as rosas brancas que existem realmente nas roseiras. Sucedeu que todas as rosas brancas se pareciam exactamente com a rosa que ela bordara, que cada uma delas era exactamente aquela. Ela levou o trabalho ao palácio e é de supor que casasse com o príncipe. No fabulário, onde vem, esta fábula não traz moralidade. Mesmo porque, na idade de ouro, as fábulas não tinham moralidade nenhuma.', 'Senão quando, estando eu ocupado em preparar e apurar a minha invenção, recebi em cheio um golpe de ar; adoeci logo, e não me tratei. Tinha o emplasto no cérebro; trazia comigo a idéia fixa dos doidos e dos fortes. Via-me, ao longe, ascender do chão das turbas, e remontar ao Céu, como uma águia imortal, e não é diante de tão excelso espetáculo que um homem pode sentir a dor que o punge. No outro dia estava pior; tratei-me enfim, mas incompletamente, sem método, nem cuidado, nem persistência; tal foi a origem do mal que me trouxe à eternidade. Sabem já que morri numa sexta-feira, dia aziago, e creio haver provado que foi a minha invenção que me matou. Há demonstrações menos lúcidas e não menos triunfantes. Não era impossível, entretanto, que eu chegasse a galgar o cimo de um século, e a figurar nas folhas públicas, entre macróbios. Tinha saúde e robustez. Suponha-se que, em vez de estar lançando os alicerces de uma invenção farmacêutica, tratava de coligir os elementos de uma instituição política, ou de uma reforma religiosa. Vinha a corrente de ar, que vence em eficácia o cálculo humano, e lá se ia tudo. Assim corre a sorte dos homens.'],[4.507142857142857, 0.6928571428571428, 0.55, 57.36363636363637, 1.8181818181818181, 31.55])
+#print(teste)
+
+'''
+texto1 = "Voltei-me para ela; Capitu tinha os olhos no chão. Ergueu-os logo, devagar, e ficamos a olhar um para o outro... Confissão de crianças, tu valias bem duas ou três páginas, mas quero ser poupado. Em verdade, não falamos nada; o muro falou por nós. Não nos movemos, as mãos é que se estenderam pouco a pouco, todas quatro, pegando-se, apertando-se, fundindo-se. Não marquei a hora exata daquele gesto. Devia tê-la marcado; sinto a falta de uma nota escrita naquela mesma noite, e que eu poria aqui com os erros de ortografia que trouxesse, mas não traria nenhum, tal era a diferença entre o estudante e o adolescente. Conhecia as regras do escrever, sem suspeitar as do amar; tinha orgias de latim e era virgem de mulheres."
+texto2 = "Senão quando, estando eu ocupado em preparar e apurar a minha invenção, recebi em cheio um golpe de ar; adoeci logo, e não me tratei. Tinha o emplasto no cérebro; trazia comigo a idéia fixa dos doidos e dos fortes. Via-me, ao longe, ascender do chão das turbas, e remontar ao Céu, como uma águia imortal, e não é diante de tão excelso espetáculo que um homem pode sentir a dor que o punge. No outro dia estava pior; tratei-me enfim, mas incompletamente, sem método, nem cuidado, nem persistência; tal foi a origem do mal que me trouxe à eternidade. Sabem já que morri numa sexta-feira, dia aziago, e creio haver provado que foi a minha invenção que me matou. Há demonstrações menos lúcidas e não menos triunfantes. Não era impossível, entretanto, que eu chegasse a galgar o cimo de um século, e a figurar nas folhas públicas, entre macróbios. Tinha saúde e robustez. Suponha-se que, em vez de estar lançando os alicerces de uma invenção farmacêutica, tratava de coligir os elementos de uma instituição política, ou de uma reforma religiosa. Vinha a corrente de ar, que vence em eficácia o cálculo humano, e lá se ia tudo. Assim corre a sorte dos homens."
+texto3 = "Num fabulário ainda por encontrar será um dia lida esta fábula: A uma bordadora dum país longínquo foi encomendado pela sua rainha que bordasse, sobre seda ou cetim, entre folhas, uma rosa branca. A bordadora, como era muito jovem, foi procurar por toda a parte aquela rosa branca perfeitíssima, em cuja semelhança bordasse a sua. Mas sucedia que umas rosas eram menos belas do que lhe convinha, e que outras não eram brancas como deviam ser. Gastou dias sobre dias, chorosas horas, buscando a rosa que imitasse com seda, e, como nos países longínquos nunca deixa de haver pena de morte, ela sabia bem que, pelas leis dos contos como este, não podiam deixar de a matar se ela não bordasse a rosa branca. Por fim, não tendo melhor remédio, bordou de memória a rosa que lhe haviam exigido. Depois de a bordar foi compará-la com as rosas brancas que existem realmente nas roseiras. Sucedeu que todas as rosas brancas se pareciam exactamente com a rosa que ela bordara, que cada uma delas era exactamente aquela. Ela levou o trabalho ao palácio e é de supor que casasse com o príncipe. No fabulário, onde vem, esta fábula não traz moralidade. Mesmo porque, na idade de ouro, as fábulas não tinham moralidade nenhuma."
+texto4 = texto1 = "Então resolveu ir brincar com a Máquina pra ser também imperador dos filhos da mandioca. Mas as três cunhas deram muitas risadas e falaram que isso de deuses era gorda mentira antiga, que não tinha deus não e que com a máquina ninguém não brinca porque ela mata. A máquina não era deus não, nem possuía os distintivos femininos de que o herói gostava tanto. Era feita pelos homens. Se mexia com eletricidade com fogo com água com vento com fumo, os homens aproveitando as forças da natureza. Porém jacaré acreditou? nem o herói! Se levantou na cama e com um gesto, esse sim! bem guaçu de desdém, tó! batendo o antebraço esquerdo dentro do outro dobrado, mexeu com energia a munheca direita pras três cunhas e partiu. Nesse instante, falam, ele inventou o gesto famanado de ofensa: a pacova."
+ass1 = [4.507142857142857, 0.6928571428571428, 0.55, 70.81818181818181, 1.8181818181818181, 38.5]
+ass2 = [4.507142857142857, 0.6928571428571428, 0.55, 57.36363636363637, 1.8181818181818181, 31.55]
+'''
+
+# Testando compara_assinatura ******************************************************
+#comp_assinatura = compara_assinatura (ass1, ass2)
+#print(comp_assinatura)
+
+# Testando calcula_assinatura *******************************************************
+
+# print(calcula_assinatura(texto))
 # [4.507142857142857, 0.6928571428571428, 0.55, 70.81818181818181, 1.8181818181818181, 38.5]
+
+#['Então resolveu ir brincar com a Máquina pra ser também imperador dos filhos da mandioca. Mas as três cunhas deram muitas risadas e falaram que isso de deuses era gorda mentira antiga, que não tinha deus não e que com a máquina ninguém não brinca porque ela mata. A máquina não era deus não, nem possuía os distintivos femininos de que o herói gostava tanto. Era feita pelos homens. Se mexia com eletricidade com fogo com água com vento com fumo, os homens aproveitando as forças da natureza. Porém jacaré acreditou? nem o herói! Se levantou na cama e com um gesto, esse sim! bem guaçu de desdém, tó! batendo o antebraço esquerdo dentro do outro dobrado, mexeu com energia a munheca direita pras três cunhas e partiu. Nesse instante, falam, ele inventou o gesto famanado de ofensa: a pacova.', 'Voltei-me para ela; Capitu tinha os olhos no chão. Ergueu-os logo, devagar, e ficamos a olhar um para o outro... Confissão de crianças, tu valias bem duas ou três páginas, mas quero ser poupado. Em verdade, não falamos nada; o muro falou por nós. Não nos movemos, as mãos é que se estenderam pouco a pouco, todas quatro, pegando-se, apertando-se, fundindo-se. Não marquei a hora exata daquele gesto. Devia tê-la marcado; sinto a falta de uma nota escrita naquela mesma noite, e que eu poria aqui com os erros de ortografia que trouxesse, mas não traria nenhum, tal era a diferença entre o estudante e o adolescente. Conhecia as regras do escrever, sem suspeitar as do amar; tinha orgias de latim e era virgem de mulheres.', 'Num fabulário ainda por encontrar será um dia lida esta fábula: A uma bordadora dum país longínquo foi encomendado pela sua rainha que bordasse, sobre seda ou cetim, entre folhas, uma rosa branca. A bordadora, como era muito jovem, foi procurar por toda a parte aquela rosa branca perfeitíssima, em cuja semelhança bordasse a sua. Mas sucedia que umas rosas eram menos belas do que lhe convinha, e que outras não eram brancas como deviam ser. Gastou dias sobre dias, chorosas horas, buscando a rosa que imitasse com seda, e, como nos países longínquos nunca deixa de haver pena de morte, ela sabia bem que, pelas leis dos contos como este, não podiam deixar de a matar se ela não bordasse a rosa branca. Por fim, não tendo melhor remédio, bordou de memória a rosa que lhe haviam exigido. Depois de a bordar foi compará-la com as rosas brancas que existem realmente nas roseiras. Sucedeu que todas as rosas brancas se pareciam exactamente com a rosa que ela bordara, que cada uma delas era exactamente aquela. Ela levou o trabalho ao palácio e é de supor que casasse com o príncipe. No fabulário, onde vem, esta fábula não traz moralidade. Mesmo porque, na idade de ouro, as fábulas não tinham moralidade nenhuma.', 'Senão quando, estando eu ocupado em preparar e apurar a minha invenção, recebi em cheio um golpe de ar; adoeci logo, e não me tratei. Tinha o emplasto no cérebro; trazia comigo a idéia fixa dos doidos e dos fortes. Via-me, ao longe, ascender do chão das turbas, e remontar ao Céu, como uma águia imortal, e não é diante de tão excelso espetáculo que um homem pode sentir a dor que o punge. No outro dia estava pior; tratei-me enfim, mas incompletamente, sem método, nem cuidado, nem persistência; tal foi a origem do mal que me trouxe à eternidade. Sabem já que morri numa sexta-feira, dia aziago, e creio haver provado que foi a minha invenção que me matou. Há demonstrações menos lúcidas e não menos triunfantes. Não era impossível, entretanto, que eu chegasse a galgar o cimo de um século, e a figurar nas folhas públicas, entre macróbios. Tinha saúde e robustez. Suponha-se que, em vez de estar lançando os alicerces de uma invenção farmacêutica, tratava de coligir os elementos de uma instituição política, ou de uma reforma religiosa. Vinha a corrente de ar, que vence em eficácia o cálculo humano, e lá se ia tudo. Assim corre a sorte dos homens.']
